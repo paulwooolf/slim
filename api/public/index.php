0b6f5b05157bff08553037a9bdfb7ba949dc8ff1
paulwooolf <paulwooolf@gmail.com>
2021-03-17 02:07:44 +0300

    feat: add DI container

--- api/public/index.php
+++ api/public/index.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use DI\Container;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, array $args) {
