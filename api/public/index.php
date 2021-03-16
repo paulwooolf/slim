f56667be867f256371452a68d5263880eadce7e5
paulwooolf <paulwooolf@gmail.com>
2021-03-17 01:29:02 +0300

    feat: add slim framework

--- api/public/index.php
+++ api/public/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'api';
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+   $response->getBody()->write("Hello world from Slim App");
+   return $response;
+});
+
+$app->run();
